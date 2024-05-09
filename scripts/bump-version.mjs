fac82b5124c554cd942bd84c8bc2bb57021aebaf
Valentin Cocaud <v.cocaud@gmail.com>
2024-05-09 20:55:14 +0200

    :rocket: add firefox update manifest for auto-updates

--- scripts/bump-version.mjs
+++ scripts/bump-version.mjs
@@ -18,4 +18,16 @@ const manifest = JSON.parse(fs.readFileSync('manifest.json'))
 manifest.version = JSON.parse(fs.readFileSync('package.json')).version
 fs.writeFileSync('manifest.json', JSON.stringify(manifest, null, 2))
 
+const firefoxUpdates = JSON.parse(
+  fs.readFileSync('hosting/firefox/updates.json'),
+)
+firefoxUpdates['web-extension@the-guild.dev'].updates.unshift({
+  version: manifest.version,
+  update_link: `https://github.com/EmrysMyrddin/the-guild-web-extension/releases/download/${manifest.version}/firefox.xpi`,
+})
+fs.writeFileSync(
+  'hosting/firefox/updates.json',
+  JSON.stringify(firefoxUpdates, null, 2),
+)
+
 await $`yarn run pack`
